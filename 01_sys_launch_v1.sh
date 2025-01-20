cb9a9d4b28c868f8e374826230b6ff372e5b7dc2
geommax <geom.xxxxxx@gmail.com>
2025-01-20 22:46:50 +0630

    update

--- 01_sys_launch_v1.sh
+++ 01_sys_launch_v1.sh
@@ -17,6 +17,8 @@ set_system_language() {
   echo 'LC_ALL=en_US.UTF-8' | sudo tee -a /etc/default/locale > /dev/null
   echo 'LANGUAGE=en_US.UTF-8' | sudo tee -a /etc/default/locale > /dev/null
   sudo locale-gen
+  echo "System Language is changed ! "
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 }
 
 # Function to set timezone and enable NTP
@@ -27,6 +29,11 @@ set_timezone() {
   ##sudo apt-get install -y ntp
   sudo service ntp start
   ##sudo timedatectl set-ntp true
+  echo "System timezone is changed to $TIMEZONE..."
+  echo "Current Date & Time is " 
+  date
+  sleep 3;
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 }
 
 # Function to install Myanmar fonts and keyboard configuration
@@ -45,6 +52,10 @@ XKBVARIANT=","
 XKBOPTIONS="grp:alt_shift_toggle,terminate:ctrl_alt_bksp,grp_led:scroll"
 BACKSPACE="guess"
 EOF
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+  echo "enter ko pae press twar par !.."
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+  sleep 3;
   sudo dpkg-reconfigure keyboard-configuration
   sudo systemctl restart keyboard-setup
 }
@@ -52,14 +63,17 @@ EOF
 # Function to configure XFCE4
 setup_xfce4() {
   echo "Setting up XFCE4 configuration..."
-  sudo -u "$USER" rm -r /home/$USER/.config/
+  sudo -u "$USER" rm -r /home/$USER/.config/xfce4
   sudo -u "$USER" cp -rv ./xfce4 /home/$USER/.config/
   sudo -u "$USER" cp -rv wallpaper.png /home/$USER/
+  echo "XFCE4 is finished successfully !..."
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 }
 
 # Function to set up kiosk mode
 setup_kiosk_mode() {
   echo "Setting up Chromium kiosk mode autostart..."
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   sudo -u "$USER" mkdir -p "$AUTOSTART_DIR"
   cat <<EOF | sudo -u "$USER" tee "$AUTOSTART_DIR/chromium-kiosk.desktop" > /dev/null
 [Desktop Entry]
@@ -70,6 +84,7 @@ X-GNOME-Autostart-enabled=true
 EOF
 
   echo "Disabling power management settings..."
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
   sudo -u "$USER" mkdir -p /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml
   cat <<EOF | sudo -u "$USER" tee /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml > /dev/null
 <?xml version="1.0" encoding="UTF-8"?>
@@ -82,6 +97,8 @@ EOF
 EOF
 
   echo "Disabling mouse cursor..."
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+  sudo apt install libglib2.0-bin unclutter -y
   echo "CLUTTER_CURSOR_SIZE=0" | sudo tee -a /etc/environment
   cat <<EOF | sudo -u "$USER" tee "$AUTOSTART_DIR/unclutter.desktop" > /dev/null
 [Desktop Entry]
@@ -99,6 +116,9 @@ main() {
   install_myanmar_fonts_and_keyboard
   setup_xfce4
   setup_kiosk_mode
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+  echo " Right Click Function ko close par !.."
+  echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
   echo "Setup is complete! Reboot the system to apply changes. (y/n)"
   read -r REBOOT
   if [[ $REBOOT == "y" || $REBOOT == "Y" ]]; then
--- 02_app_deploy.sh
+++ 02_app_deploy.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
 echo "Deployment script started..."
+
+# printer
+# network
+# mariadb install, apache2, php8.4
+
+# copying and restarting related service.
+# Test Print 
+
+sudo apt install xinput
+
+
+c/X11/xorg.conf.d$ sudo nano /etc/X11/xorg.conf.d/90-disable-cursor.conf
+sudo: nano: command not found
+linaro@linaro-alip:/etc/X11/xorg.conf.d$ sudo vim 90-disable-crusor.conf
+
+Section "InputClass"
+    Identifier "Disable Mouse"
+    MatchIsPointer "on"
+    Option "PointerDisable" "true"
+EndSection
+
+
+root@linaro-alip:/home/linaro# xinput list
+⎡ Virtual core pointer                    	id=2	[master pointer  (3)]
+⎜   ↳ Virtual core XTEST pointer              	id=4	[slave  pointer  (2)]
+⎜   ↳ Instant USB Optical Mouse               	id=7	[slave  pointer  (2)]
+⎜   ↳ SONiX USB Keyboard Consumer Control     	id=9	[slave  pointer  (2)]
+⎜   ↳ goodix-ts                               	id=13	[slave  pointer  (2)]
+⎣ Virtual core keyboard                   	id=3	[master keyboard (2)]
+    ↳ Virtual core XTEST keyboard             	id=5	[slave  keyboard (3)]
+    ↳ adc-keys                                	id=6	[slave  keyboard (3)]
+    ↳ SONiX USB Keyboard                      	id=8	[slave  keyboard (3)]
+    ↳ SONiX USB Keyboard System Control       	id=10	[slave  keyboard (3)]
+    ↳ rk805 pwrkey                            	id=11	[slave  keyboard (3)]
+    ↳ hdmi_cec_key                            	id=12	[slave  keyboard (3)]
+    ↳ SONiX USB Keyboard Consumer Control     	id=14	[slave  keyboard (3)]
+root@linaro-alip:/home/linaro#     
+
